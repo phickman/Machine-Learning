8737e5145ba624d832060bd3fcc4631572ec961f
Paul Hickman <Paul.Hickman@conocophillips.com>
2016-06-27 13:32:16 +0800

    Completed polynomial regression

--- Week 6 - Regularized Linear Regression and Bias-Variance/ex5/validationCurve.m
+++ Week 6 - Regularized Linear Regression and Bias-Variance/ex5/validationCurve.m
@@ -40,7 +40,18 @@ error_val = zeros(length(lambda_vec), 1);
 %
 
 
-
+for i = 1:length(lambda_vec)
+  lambda = lambda_vec(i);
+  theta = trainLinearReg(X, y, lambda)
+  
+  % Compute train / val errors when training linear 
+  % regression with regularization parameter lambda
+  % You should store the result in error_train(i)
+  error_train(i) = linearRegCostFunction(X, y, theta, 0);
+  % and error_val(i)
+  error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+  
+end
 
 
 
